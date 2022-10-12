f23bb48cc9c3e809d744656212cba7eb32fa55a7
Francisco Meneses <francisco.meneses.villegas@gmail.com>
2022-10-12 00:01:27 -0300

    agregados cambios finales

--- baseDatos.sql
+++ baseDatos.sql
@@ -1,32 +1,28 @@
-create table estados (
-	county text not null,
-	state text not null,
-	state_code text not null,
-	population int not null,
-	primary key (county, state)
-);
-
-create table condados (
-	fips int not null,
-	county text not null,
-	state text not null,
-	state_code text not null,
-	male int not null,
-	female int not null,
+create table estados(
+	state_name text not null primary key,
+	state_code text not null
+)
+;
+create table condados(
+	fips text not null primary key,
+	county_name text not null,
+	male_pop int not null,
+	female_pop int not null,
 	median_age float not null,
-	lat float not null,
-	long float not null,
-	primary key (county, state),
-	foreign key (county, state) references estados (county, state)
-);
-
-create table diarios (
-	date date not null,
-	county text not null,
-	state text not null,
-	fips int not null,
+	latitude float not null,
+	longitude float not null,
+	population int not null
+)
+;
+create table diarios(
+	date_ date not null,
+	fips text not null references condados(fips),
+	state_name text not null,
 	cases int not null,
 	deaths int not null,
-	primary key (date, county, state),
-	foreign key (county, state) references estados (county, state)
-);
+	primary key(date_, fips)
+)
+
+drop table diarios;
+drop table condados;
+drop table estados;
